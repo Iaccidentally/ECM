59531d74254287bd475cdcb1ee21b0e685550fc7
Gavin Casdorph <whisk@w-c.us>
2012-05-19 15:13:55 -0800

    Added the Util class for Debug, etc.

---
+++ src/com/github/Iaccidentally/ECM/commands/Util.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.github.Iaccidentally.ECM.commands;
+
+/**
+ *
+ * @author Whisk
+ */
+public class Util {
+
+}
