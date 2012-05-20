355d59b347d9c45827e43a19fa8248ef85b76c01
Gavin Casdorph <whisk@w-c.us>
2012-05-20 12:20:28 -0800

    Added getString

--- src/com/github/Iaccidentally/ECM/configuration/Configuration.java
+++ src/com/github/Iaccidentally/ECM/configuration/Configuration.java
@@ -5,10 +5,22 @@
 
 package com.github.Iaccidentally.ECM.configuration;
 
+import com.github.Iaccidentally.ECM.CommandManager;
+
 /**
  *
  * @author Whisk
  */
 public class Configuration {
+    
+    /**
+     * Returns configuration data from the node
+     * @param node Configuration node
+     * @return String to be returned
+     */
+    public static String getString(String node) {
+        String stringToRetrun = CommandManager.getPlugin().getConfig().getString(node);
+        return stringToReturn;
+    }
 
 }
