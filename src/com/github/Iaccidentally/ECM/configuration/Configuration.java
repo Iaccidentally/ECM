f868c88e2cbc62dd1c888e223abaea36c22afc03
Gavin Casdorph <whisk@w-c.us>
2012-05-20 12:16:45 -0800

    Added the Configuration class.

---
+++ src/com/github/Iaccidentally/ECM/configuration/Configuration.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.github.Iaccidentally.ECM.configuration;
+
+/**
+ *
+ * @author Whisk
+ */
+public class Configuration {
+
+}
