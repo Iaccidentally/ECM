bd11f446c051782cc22089c4aa9c3e6ab7bddaf4
Gavin Casdorph <whisk@w-c.us>
2012-05-19 23:09:00 -0800

    Added some for language and configuragion.

--- src/com/github/Iaccidentally/ECM/ECM.java
+++ src/com/github/Iaccidentally/ECM/ECM.java
@@ -4,9 +4,15 @@
  */
 package com.github.Iaccidentally.ECM;
 
+import com.github.Iaccidentally.ECM.commands.Util;
 import com.github.Iaccidentally.ECM.listeners.BlockListener;
 import com.github.Iaccidentally.ECM.listeners.PlayerListener;
+import java.io.File;
+import java.io.InputStream;
 import java.util.logging.Logger;
+import org.bukkit.configuration.Configuration;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -23,6 +29,9 @@ public class ECM extends JavaPlugin {
     private PlayerListener PlayerListener = new PlayerListener();
     private BlockListener BlockListener = new BlockListener();
     
+    private FileConfiguration data = null, language = null;
+    private File dataFile = null, languageFile = null;
+    
     @Override
     public void onEnable() {
         this.log.info("ECM is now enabled!");
@@ -37,4 +46,49 @@ public class ECM extends JavaPlugin {
         this.log.info("ECM is now disabled!");
         
     }
+    
+    public void reloadData() {
+        if (dataFile == null) {
+            dataFile = new File(getDataFolder(), "data.yml");
+            
+        }
+        data = YamlConfiguration.loadConfiguration(dataFile);
+        
+        InputStream defConfigStream = getResource("data.yml");
+        if (defConfigStream !=null) {
+            YamlConfiguration defConfig = YamlConfiguration.loadConfiguration(defConfigStream);
+            data.setDefaults(defConfig);
+        }
+        
+    }
+    public FileConfiguration getData() {
+        if (data == null) {
+            reloadData();
+        }
+        return data;
+        
+    }
+    public void reloadLanguage() {
+        
+            String lang = Configuration.getString("configuration.language") + ".yml";
+            Util.log("Language file used: " + lang);
+        if (languageFile == null) {
+        languageFile = new File(getDataFolder(), lang);
+        
+        }
+        
+        InputStream defConfigStream = getResource(lang);
+        if (defConfigStream !=null) {
+            YamlConfiguration defConfig = YamlConfiguration.loadConfiguration(defConfigStream);
+            language.setDefaults(defConfig);
+        }
+    }
+    
+    public FileConfiguration getLanguage() {
+        if (language == null) {
+            reloadLanguage();
+        }
+        return language;
+    }
+    
 }
