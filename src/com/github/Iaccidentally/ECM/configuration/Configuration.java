22ed770640c4901ab7d03f003ecb634c1b489e85
Gavin Casdorph <whisk@w-c.us>
2012-05-20 12:23:15 -0800

    Added getInt and getBoolean

--- src/com/github/Iaccidentally/ECM/configuration/Configuration.java
+++ src/com/github/Iaccidentally/ECM/configuration/Configuration.java
@@ -22,5 +22,25 @@ public class Configuration {
         String stringToRetrun = CommandManager.getPlugin().getConfig().getString(node);
         return stringToReturn;
     }
+    
+    /**
+     * Returns configuration data from the node
+     * @param node Configuration node
+     * @return boolean to be returned
+     */
+    public static boolean getBoolean(String node) {
+        boolean booleanToReturn = CommandManager.getPlugin().getConfig().getBoolean(node);
+        return booleanToReturn;
+    }
+    
+    /**
+     * Returns configuration data from the node
+     * @param node Configuration node
+     * @retun int to be returned
+     */
+    public static int getInt(String node) {
+        int intToReturn = CommandManager.getPlugin().getConfig().getInt(node);
+        return intToReturn;
+    }
 
 }
