dbf7995e5049874eccbfda26685a02e2216ee365
Iaccidentally <coryhuckaby@gmail.com>
2012-05-20 22:38:01 -0400

    this is actually the playerlistener

--- src/main/java/com/github/iaccidentally/ecm/listeners/PlayerListener.java
+++ src/main/java/com/github/iaccidentally/ecm/listeners/PlayerListener.java
@@ -6,8 +6,39 @@ package com.github.Iaccidentally.ECM.listeners;
 
 /**
  *
- * @author casdorph.gavin
+ * @author casdorph.gavin & Iaccidentally
  */
-public class PlayerListener {
-    
+import org.bukkit.event.Listener;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerItemHeldEvent; 
+
+public class PlayerListener implements Listener {
+
+    //right click
+    //without edit mode on
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void normal(PlayerInteractEvent event) {
+        // Some code here
+    }
+
+    //with edit mode on
+    @EventHandler(priority = EventPriority.HIGH)
+    public void high(PlayerInteractEvent event) {
+        // Some code here
+    }
+
+    //item held
+    //without edit mode on
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void normal(PlayerItemHeldEvent event) {
+        // Some code here
+    }
+
+    //with edit mode on
+    @EventHandler(priority = EventPriority.HIGH)
+    public void high(PlayerItemHeldEvent event) {
+        // Some code here
+    }
 }
