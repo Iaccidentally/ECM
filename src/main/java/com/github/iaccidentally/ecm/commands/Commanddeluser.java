016f23768e29296f1824aa748e5ea6061137840b
Joshua D. Katz <gravypod@ymail.com>
2012-05-21 19:39:48 -0700

    Added SUPER BUGGY addUser command
    DO NOT PULL! I dont like the way this works and I want to add a few
    classes so if we need data that I needed we can get it easy!

--- src/main/java/com/github/iaccidentally/ecm/commands/Commanddeluser.java
+++ src/main/java/com/github/iaccidentally/ecm/commands/Commanddeluser.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.github.Iaccidentally.ECM.commands;
+package com.github.iaccidentally.ecm.commands;
 
 /**
  *
--- src/main/java/com/github/iaccidentally/ecm/listeners/PlayerListener.java
+++ src/main/java/com/github/iaccidentally/ecm/listeners/PlayerListener.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.github.Iaccidentally.ECM.listeners;
+package com.github.iaccidentally.ecm.listeners;
 
 /**
  *
