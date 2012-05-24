110f4223a6bfe5b4eeba8a1034dcced99c6a3f7d
Whisk <whisk@w-c.us>
2012-05-23 18:21:16 -0800

    added senderHasPermission

--- src/main/java/com/github/iaccidentally/ecm/commands/Util.java
+++ src/main/java/com/github/iaccidentally/ecm/commands/Util.java
@@ -5,8 +5,12 @@
 
 package com.github.iaccidentally.ecm.commands;
 
+import com.github.iaccidentally.ecm.CommandManager;
+import com.github.iaccidentally.ecm.configuration.Configuration;
 import org.bukkit.block.Block;
 import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.util.BlockVector;
 
 /**
@@ -34,5 +38,40 @@ public class Util {
     public static BlockVector toVector(Block block) {
         throw new UnsupportedOperationException("Not yet implemented");
     }
-
+    /**
+     * Checks if the command sender has a permission.
+     * @param node The permissions node.
+     * @return true is the sender has a permission, false if not.
+     */
+    public static boolean senderHasPermission(String node) {
+        CommandSender sender = CommandManager.getSender();
+        boolean usePermissions = Configuration.getBoolean("configuration.use-permissions");
+        
+        // Console always has the permissions
+        Player player;
+        if (isPlayer()) player = (Player) sender;
+        else return true;
+        
+        // If permissions are not used, check for op
+        if(!usePermissions) { 
+            if(player.isOp()) return true;
+            else return false;
+        }
+        
+        if(player.hasPermission("ecm."))
+            return true;
+        return false;
+    }
+    
+    /**
+     * Checks if the command sender has a permission.
+     * @param node The permission node
+     * @return true is the sender has a permission,  false if not.
+     * 
+     */
+    public static boolean playerHasPermission(Player player, String node) {
+        boolean usePermissions = Configuration.getBoolean("configuration.use-permissions");
+        
+        if(!usePermissions)
+    }
 }
