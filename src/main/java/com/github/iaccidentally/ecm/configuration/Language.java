e84a18a3d0bcd92861cf232231dd06d4d34bdaec
Whisk1 <whisk@w-c.us>
2012-05-30 16:54:17 -0800

    New class eventually for languages.

---
+++ src/main/java/com/github/iaccidentally/ecm/configuration/Language.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.iaccidentally.ecm.configuration;
+
+/**
+ *
+ * @author gavincasdorph
+ */
+public class Language {
+    
+}
